39b2feabc49718143333e8f0742946823ac2bc94
sourav louha <souravlouha00@gail.com>
2024-09-04 18:10:12 +0530

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  // Here you can add code to handle form submission, like sending data to a server
+  alert('Thank you for your message! We will get back to you soon.');
+});
